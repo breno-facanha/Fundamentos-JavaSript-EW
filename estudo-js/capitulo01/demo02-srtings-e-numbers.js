bb8f248515b10076450637766d8546aa9589c943
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2021-12-04 13:12:10 -0300

    Variaveis Strings, Numbers e Operadores

---
+++ estudo-js/capitulo01/demo02-srtings-e-numbers.js
@@ -0,0 +1,13 @@
+let salarioDoAmigo = 1000
+let meuSalario = "2999"
+let meuSalarioConvertido = Number(meuSalario)
+
+console.log(salarioDoAmigo + meuSalarioConvertido)
+console.log(
+    typeof(salarioDoAmigo),
+    salarioDoAmigo
+)
+console.log(
+    typeof(meuSalario),
+    meuSalario
+)
---
+++ estudo-js/capitulo01/demo03-operadores.js
@@ -0,0 +1,13 @@
+console.log('OPERADORES ARITIMETICOS')
+console.log(" 1 + 1 é : ", 1+1)
+console.log(' 2 - 4 é : ', 2-4)
+console.log(' 2 * 4 é : ', 2*4)
+console.log(' 2 / 4 é : ', 2/4)
+console.log(' 2 % 4 é : ', 2%4)
+console.log('2 ** 4 é : ', 2**4)
+console.log('-----------------------')
+
+console.log('OPERADORES COMPARAÇÕES')
+console.log(' 1 > 2 é :', 1>2)
+console.log(' 2 < 4 é :', 2>4)
+console.log(' ') 
