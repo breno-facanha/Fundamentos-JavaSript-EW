e5b59b1807b613b77fd251090a636a63ae8134d1
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2021-12-04 13:32:53 -0300

    demo04 - comentarios de codigo
    comentarios serve para
    - descrever um codigo
    - igonorar um codigo
    - colocar observações

---
+++ estudo-js/capitulo01/demo04-comentarios.js
@@ -0,0 +1,15 @@
+// comentário em uma unica linha 
+
+/*
+comentando varias linhas 
+*/
+
+
+/*
+comentarios serve para 
+- descrever um codigo 
+- igonorar um codigo
+- colocar observações 
+
+Finalidade é que fique o mais entendivel possivel do que voce fez
+*/
