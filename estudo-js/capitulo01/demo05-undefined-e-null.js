25c76021ae23b17def567b4fb6193d819746819f
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2021-12-04 13:40:29 -0300

    demo05 undefined e null
    Diferença entre undefined e null

---
+++ estudo-js/capitulo01/demo05-undefined-e-null.js
@@ -0,0 +1,21 @@
+// Variavel só inicializada sem valor elá é UNDEFINED
+let variavelSemInicalizacao
+console.log(
+    variavelSemInicalizacao,
+    typeof(variavelSemInicalizacao)
+)
+
+
+//Variavel inicializada com um valor, nesse caso recebe Hello Word do tipo String
+variavelSemInicalizacao = 'Hello Word !'
+console.log(
+    variavelSemInicalizacao,
+    typeof(variavelSemInicalizacao)
+)
+
+//Variavel recebdno um valor null ela está vazia, porém com o tipo object
+variavelSemInicalizacao = null
+console.log(
+    variavelSemInicalizacao,
+    typeof(variavelSemInicalizacao)
+)
